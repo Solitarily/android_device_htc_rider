bd0f5843c1245478bf44b0f73fbf49f542b6e1ec
luomu <chuangweida@gmail.com>
2014-04-07 23:31:02 +0800

    jellybean

--- aosp.mk
+++ aosp.mk
@@ -1,14 +1,14 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/ev/config/gsm.mk)
+$(call inherit-product, vendor/aosp/config/gsm.mk)
 
 # Inherit some common evervolv stuff.
-$(call inherit-product, vendor/ev/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/rider/device_rider.mk)
 
 # Device naming
-PRODUCT_NAME := ev_rider
+PRODUCT_NAME := aosp_rider
 PRODUCT_DEVICE := rider
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := HTC X515E
@@ -17,9 +17,4 @@ PRODUCT_MANUFACTURER := HTC
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_rider BUILD_FINGERPRINT=tmous/htc_rider/rider:4.0.3/IML74K/356011.14:user/release-keys PRIVATE_BUILD_DESC="3.32.531.14 CL356011 release-keys" BUILD_NUMBER=356011
 
-#
-# Set up the product codename, build version & MOTD.
-#
-PRODUCT_CODENAME := Macto
-PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
